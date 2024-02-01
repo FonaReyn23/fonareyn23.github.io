4fdfe33fff51d3f441cc609cfbb635047b3ec2b1
FonaReyn23 <105812160+FonaReyn23@users.noreply.github.com>
2024-02-01 20:56:21 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
